bd5f7856aea256816263e7e64f5eed91f0a9f945
cbj252 <cbj252@gmail.com>
2022-11-26 20:20:43 +0800

    Updates

---
+++ precache-manifest.2cd51eb355b837c4c352d395bf42e24f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "915704cef3e6aa189fb2",
+    "url": "/forum_frontend/static/css/main.009eceee.chunk.css"
+  },
+  {
+    "revision": "915704cef3e6aa189fb2",
+    "url": "/forum_frontend/static/js/main.f05d44d7.chunk.js"
+  },
+  {
+    "revision": "bdc32b5ac396833a97cc",
+    "url": "/forum_frontend/static/js/runtime~main.ce8be88a.js"
+  },
+  {
+    "revision": "3030c5c1a21f3cfebdd4",
+    "url": "/forum_frontend/static/js/2.d1bf0ddb.chunk.js"
+  },
+  {
+    "revision": "9a5629915b0d4fbc94ddeed5ed68e89c",
+    "url": "/forum_frontend/index.html"
+  }
+];
